93ea064bbd11b9bf3aff44438506ef7caee338c9
alejo herrera <85969@sistemas.frc.utn.edu.ar>
2023-10-27 02:31:43 -0300

    boundary CASI terminado

--- src/main/java/com/example/tercerentrega_ppai/HelloApplication.java
+++ src/main/java/com/example/tercerentrega_ppai/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.tercerentrega_ppai;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,9 +15,13 @@ public class HelloApplication extends Application {
 
         //Scene es el main component de javaFX
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Consultar llamadas");
         stage.setScene(scene);
         stage.show();
+
+        //Setear icono
+        Image img = new Image("C:\\Users\\Alejo\\Documents\\tercerentrega_ppai\\src\\main\\resources\\media\\img-logo.png");
+        stage.getIcons().add(img);
     }
 
     public static void main(String[] args) {
